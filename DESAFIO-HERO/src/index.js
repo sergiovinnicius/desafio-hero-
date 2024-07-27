ac04602ac22541a93180e562fb67efd923f1d030
Sérgio Vinnícius <47503741+sergiovinnicius@users.noreply.github.com>
2024-07-27 14:41:23 -0400

    Add files via upload

---
+++ DESAFIO-HERO/src/index.js
@@ -0,0 +1,45 @@
+//Declaração Variáveis
+
+let nameHero = "Sérgio"
+let XP = 7.000
+
+//ESTRUTURA DE DECISÃO
+if (XP < 1.000) {
+
+    console.log("O herói " + nameHero, "está no nível Ferro" )
+
+  } else if((XP > 1.001) && ( XP < 2.000)){
+
+    console.log("O herói " + nameHero, "está no nível Bronze" )
+
+  }else if((XP > 2.001) && ( XP < 5.000)) {
+   
+    console.log("O herói " + nameHero, "está no nível Prata" )
+
+  }else if((XP > 5.001) && ( XP < 7.000)) {
+   
+    console.log("O herói " + nameHero, "está no nível Ouro" )
+
+  }else if((XP > 7.001) && ( XP < 8.000)) {
+   
+    console.log("O herói " + nameHero, "está no nível Platina" )
+
+  }else if((XP > 8.001) && ( XP < 9.000)) {
+   
+    console.log("O herói " + nameHero, "está no nível Ascendente" )
+
+  }else if((XP > 9.001) && ( XP < 10.000)) {
+   
+    console.log("O herói " + nameHero, "está no nível Imortal" )
+
+  }else if((XP ==  10.001) || ( XP > 10.000) ){
+   
+    console.log("O herói " + nameHero, "está no nível Radiante" )
+
+    
+  }
+
+ 
+   
+
+
